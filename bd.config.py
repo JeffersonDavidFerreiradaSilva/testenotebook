340c222aef42c476efa4fbd691055343ac6e34b1
Jefferson David <napsterdavid@gmail.com>
2023-08-25 09:01:30 -0300

    View pronta

---
+++ bd.config.py
@@ -0,0 +1,4 @@
+host = 'localhost'
+user = 'root'
+password = '@Cursos21'
+database = 'serpro'
---
+++ view/view_cadastro_aluno.py
@@ -0,0 +1,10 @@
+class cadastro:
+    @staticmethod
+    def pegar_dados_aluno():
+        nome = input("DIGITE SEU NOME: ")
+        idade = int(input("DIGITE SUA IDADE: "))
+        peso = float(input("DIGITE SEU PESO: "))
+        return nome, idade, peso
+    @staticmethod
+    def mostrar_menssagem(menssagem):
+        print(menssagem)        
